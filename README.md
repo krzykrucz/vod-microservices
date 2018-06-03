25c30af1b433810587bb6c0fdec4d0d92992e23f
hector <krzysiuk.66@gmail.com>
2018-06-03 11:02:00 +0200

    eureka fix

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Video-on-demand application powered by microservice architecture and created according to Domain-driven design principles.
 
+### Running
+Requirements:
+* running zipkin server
+* running mongod
+* running zookeeper
+* running kafka
+
 ![alt text](https://raw.githubusercontent.com/krzykrucz/vod-microservices/master/VoD.jpg)
 
  <br />
--- videos/src/main/java/com/krzykrucz/movies/VideosApplication.java
+++ videos/src/main/java/com/krzykrucz/movies/VideosApplication.java
@@ -2,6 +2,7 @@ package com.krzykrucz.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.FilterType;
                 })
 })
 @EnableFeignClients
+@EnableDiscoveryClient
 public class VideosApplication {
 
     public static void main(String[] args) {
